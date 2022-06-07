4597775643e84fbc93c7729d0110becc5292c335
Danielle Mapena <105190173+DeeNicole95@users.noreply.github.com>
2022-06-08 01:15:28 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,14 @@
-jh
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ *
+ * Return: Success (1) if lowercase or uppercase
+ * otherwise return (0)
+ *
+ * @c: input)
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}

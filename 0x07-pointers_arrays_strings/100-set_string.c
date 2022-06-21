0b70266abe826e697fabd13e4c1fa9f58638c636
Danielle Mapena <105190173+DeeNicole95@users.noreply.github.com>
2022-06-21 16:27:24 +0200

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,11 @@
-nnnn
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to a string
+ * @to: string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
